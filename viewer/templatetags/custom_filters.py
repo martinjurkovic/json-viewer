a9d24143b96b5cf532b2c7fa8190b7a596afe2ae
Martin Jurkovic <martin.jurkovic19@gmail.com>
2023-11-03 14:09:31 +0000

    Add bootstrap 95ci with colours for single_column

--- viewer/templatetags/custom_filters.py
+++ viewer/templatetags/custom_filters.py
@@ -5,3 +5,14 @@ register = template.Library()
 @register.filter
 def split_by_underscore(value):
     return ' ' .join([w.capitalize() for w in value.split('_')])
+
+@register.filter
+def is_in_range(value, arg):
+    # Ensure arg is a list of two values representing the range
+    if len(arg) != 2:
+        return False
+    try:
+        lower_bound, upper_bound = float(arg[0]), float(arg[1])
+        return lower_bound <= value <= upper_bound
+    except (ValueError, TypeError):
+        return False
--- viewer/views.py
+++ viewer/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 import json
 from .forms import FileUploadForm
-# Create your views here.
 
 def load_data(request):
     if request.method == 'POST':
@@ -9,8 +8,25 @@ def load_data(request):
         if form.is_valid():
             data = json.load(request.FILES['file'])
 
+            single_col_metrics = data['metrics']["single_column"]
+            for table in single_col_metrics:
+                for metric in single_col_metrics[table]:
+                    for column in single_col_metrics[table][metric]:
+                        single_col_metrics[table][metric][column]["combined_scores"] = zip(single_col_metrics[table][metric][column]["scores"],single_col_metrics[table][metric][column]["aggregation_bootstrap_scores"]["95ci"])
+
             return render(request, 'viewer/index.html', {'data': data})
     else:
         form = FileUploadForm()
 
     return render(request, 'viewer/upload.html', {'form': form})
+
+if __name__ == '__main__':
+    import os
+    print("Current working directory:", os.getcwd())
+    data = json.load(open('mutagenesis_mostlyai.json'))
+    single_col_metrics = data['metrics']["single_column"]
+    for table in single_col_metrics:
+        for metric in single_col_metrics[table]:
+            for column in single_col_metrics[table][metric]:
+                 single_col_metrics[table][metric][column]["combined_scores"] = zip(single_col_metrics[table][metric][column]["scores"],single_col_metrics[table][metric][column]["aggregation_bootstrap_scores"]["95ci"])
+
